ff869130966b6cdcd648a76bb9e66c6f2680d0f5
hammadsadi <hammad.sadi@yahoo.com>
2025-11-28 11:16:27 +0600

    CHain Comparision Pratice Done

---
+++ chain-comparision.py
@@ -0,0 +1,7 @@
+#  Chaining Comparison Operators in Python
+#  Age must be between 18 and 65 to qualify for the job.
+age = 30
+if 18 <= age <=65:
+    print("You qualify for the job.")
+else:
+    print("You do not qualify for the job.")

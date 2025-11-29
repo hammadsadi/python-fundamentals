640f8f97acffe6cdd874f599f02eae4627a8e570
hammadsadi <hammad.sadi@yahoo.com>
2025-11-29 10:36:10 +0600

    Loop Practice Done

---
+++ forloop.py
@@ -0,0 +1,12 @@
+# for loop in python 
+
+
+for number in range(3):
+    print("Sending Message", number , (number ) * ".")
+
+for number in range(1, 10):
+    print("Sending Message", number , (number ) * ".")
+
+
+for number in range(1, 10, 3):
+    print("Sending Message", number , (number ) * ".")
